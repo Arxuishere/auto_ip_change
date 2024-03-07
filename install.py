5e00d8c8302216cfb35e38e1e4dcac844036cbc3
A0R3A <125909684+Arxuishere@users.noreply.github.com>
2024-03-07 14:46:57 +0600

    install.py

--- install.py
+++ install.py
@@ -3,15 +3,15 @@ choice = input('[+] to install press (Y) to uninstall press (N) >> ')
 run = os.system
 if str(choice) =='Y' or str(choice)=='y':
 
-    run('chmod 777 autoTOR.py')
+    run('chmod 777 aip.py')
     run('mkdir /usr/share/aut')
-    run('cp aip.py /usr/share/aut/autoTOR.py')
+    run('cp aip.py /usr/share/aut/aip.py')
 
-    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/autoTOR.py "$@"')
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/aip.py "$@"')
     with open('/usr/bin/aut','w')as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/autoTOR.py')
-    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
+    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/aip.py')
+    print('''\n\ncongratulation ARXU Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
 if str(choice)=='N' or str(choice)=='n':
     run('rm -r /usr/share/aut ')
     run('rm /usr/bin/aut ')
